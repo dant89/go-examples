4356c32387928fb21307a4c9ab2731382ac7f299
Dan Thorpe <dant@linx.net>
2021-03-20 12:27:30 +0000

    Add functions with multiple return value example

---
+++ functions/functions.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	var x int = 1
+	var y int = 1
+
+	getOutput(x, "x")
+	getOutput(y, "y")
+
+	x = getExampleOne(x)
+	getOutput(x, "x")
+
+	x, y = getExampleTwo(x, y)
+	getOutput(x, "x")
+	getOutput(y, "y")
+}
+
+func getExampleOne(x int) int {
+	return x * 3
+}
+
+func getExampleTwo(x , y int) (int, int) {
+	return x, y + 1
+}
+
+func getOutput(x int, name string) {
+	fmt.Printf("%s equals %d\n", name, x)
+}
