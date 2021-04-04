ed37519f27dd19614433e88fc5c7e2cfe4db975f
Dan Thorpe <dant@linx.net>
2021-04-04 12:43:35 +0100

    Update goroutines example comment

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -9,13 +9,20 @@ func main() {
 	go subOne()
 	go subTwo()
 
-	// When the main function ends, all goroutines end.
-	//
-	// We can do a hacky fix using sleep but this should
-	// be avoided for multiple reasons. One example is that
-	// timings may change within a goroutine and then it
-	// would be ended prior to completing. Other examples
-	// will show better soloutions to this issue.
+	/*
+	When the main function ends, all goroutines end.
+	
+	We can do a hacky fix using sleep but this should
+	be avoided for multiple reasons. One example is that
+	timings may change within a goroutine and then it
+	would be ended prior to completing. Another example
+	is that the operating system may prioritise another
+	thread or goroutine first.
+
+	Timing is nondeterministic.
+	
+	Other examples will show better soloutions to this issue.
+	*/
 	time.Sleep(1 * time.Second)
 }
 
