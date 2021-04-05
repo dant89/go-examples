5113025ec74adb98f9b0a45e160f928bfaff742d
Dan Thorpe <dant@linx.net>
2021-04-06 00:29:36 +0100

    Add goroutine interleaving examples and how to prevent

--- goroutines/interleaving/interleaving.go
+++ goroutines/interleaving/interleaving.go
@@ -6,26 +6,64 @@ import (
 )
 
 var i int
+var mut sync.Mutex
 
 func main() {
-	
+	interleavingExample()
+	noneInterleavingExample()
+}
+
+func interleavingExample() {
 	var wg sync.WaitGroup
 
 	for l := 0; l < 100000; l++ {
 		i = 0
 
 		wg.Add(2)
-		go subOne(&wg)
-		go subOne(&wg)
+		go subInterleaving(&wg)
+		go subInterleaving(&wg)
 		wg.Wait()
 		
 		if (i == 1) {
-			fmt.Printf("Here is an example where i was read before being updated in both instances, attempt number %d\n", l)
+			fmt.Printf("Interleaving occured during test1 attempt %d\n", l)
 		}
 	}
 }
 
-func subOne(wg *sync.WaitGroup) {
+func noneInterleavingExample() {
+	var wg sync.WaitGroup
+	var interleaved bool = false
+
+	for l := 0; l < 100000; l++ {
+		i = 0
+
+		wg.Add(4)
+		go subNoneInterleavinig(&wg)
+		go subNoneInterleavinig(&wg)
+		go subNoneInterleavinig(&wg)
+		go subNoneInterleavinig(&wg)
+		wg.Wait()
+		
+		if (i == 1) {
+			interleaved = true
+		}
+	}
+
+	if interleaved {
+		fmt.Println("Interleaving occured during test2")
+	} else {
+		fmt.Println("Interleaving did not occur during test2")
+	}
+}
+
+func subInterleaving(wg *sync.WaitGroup) {
 	i++
+	wg.Done()
+}
+
+func subNoneInterleavinig(wg *sync.WaitGroup) {
+	mut.Lock()
+	i++
+	mut.Unlock()
 	wg.Done()
 }
